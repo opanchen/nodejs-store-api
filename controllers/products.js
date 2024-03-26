10d6bf5c70b4fcbb416d528d73238e7ed7a79379
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-03-26 18:24:29 +0100

    add manual pagination

--- controllers/products.js
+++ controllers/products.js
@@ -3,7 +3,11 @@ const Product = require("../models/product");
 const getAllProductsStatic = async (req, res) => {
   //   throw new Error("Testing async errors");
 
-  const products = await Product.find({}).select("name price");
+  const products = await Product.find({})
+    .sort("name")
+    .select("name price")
+    .limit(4)
+    .skip(1);
   res.status(200).json({ products, nbHits: products.length });
 };
 
@@ -38,6 +42,12 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(",").join(" ");
     result = result.select(fieldsList);
   }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
 
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
