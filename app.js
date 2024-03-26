e6b133df5f49d141fb85e68bd6ac53300c20767b
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-03-26 12:14:18 +0100

    connect to db

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log("Store API");
+require("dotenv").config();
+
+const express = require("express");
+
+const connectDB = require("./db/connect");
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+const app = express();
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+});
+
+// products route
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening to port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
